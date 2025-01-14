31187cd77696dde59df768493ffa4687d67fc788
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2025-01-14 18:39:37 +0100

    feat: milestone 3 completed

--- src/component/ListItems.jsx
+++ src/component/ListItems.jsx
@@ -2,6 +2,7 @@ import { FaStar, FaRegStar } from "react-icons/fa";
 
 const ListItems = ({ list }) => {
 
+    const imgCover = 'https://image.tmdb.org/t/p';  
     const stars = (vote) => Math.ceil(vote / 2);
   
      return (
@@ -9,11 +10,11 @@ const ListItems = ({ list }) => {
         {list.length > 0 ? (
           list.map((item) => (
             <div key={item.id} className="list-item">
-              <h3>{item.title || item.name}</h3>
+              <h3>{item.title || item.name}</h3> <img src={`${imgCover}/w200/${item.poster_path}`} alt="" />
               <p><strong>Titolo originale:</strong> {item.original_title || item.original_name}</p>
               <p><strong>Lingua:</strong><img className="flag"src={
-                item.original_language === "en" ? "./public/en.png" : item.original_language === "it" ? "./public/it.png"
-            : "./public/placeholder.png"} alt={`${item.original_language}`} style={{width: "1.6rem", height: "1.2rem"}} /></p>
+                item.original_language === "en" ? "./en.png" : item.original_language === "it" ? "./it.png"
+            : "./placeholder.png"} alt={`${item.original_language}`} style={{width: "1.6rem", height: "1.2rem"}} /></p>
               <p><strong>Voto:</strong> <p className="star">{""} {(() => {
                 const starsVote = stars(item.vote_average); //calcolo le stelle piene
                 const starsElement = [];
