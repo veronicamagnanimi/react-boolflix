ea434ef64aed2483e80f2ca7a5a15c4a9a83fc9d
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2025-01-14 12:11:16 +0100

    feat: milestone 0 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-function App() {
-  
+import GlobalContext from "./context/GlobalContext";
+import { useState } from "react";
 
+function App() {
+//STATE
+const [searchValue, setSearchValue] = useState("");
+
+//API
+const apiUrl = "https://api.themoviedb.org/3/search/"
+const apiKey = "4fc9382334271edcdf3039924423d9a6"
+ 
+const globalProviderValue = {
+  apiUrl, apiKey, searchValue, setSearchValue
+}
   return (
     <>
-      <h1>ciao</h1>
+     <GlobalContext.Provider value={globalProviderValue}>
+     </GlobalContext.Provider>
     </>
   )
 }
